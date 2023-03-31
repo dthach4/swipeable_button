3e3099ddb8ca5f7340c11a6cc7d75e84fa7a242c
Dany Thach <d.thach4@gmail.com>
2023-04-01 01:09:54 +0200

    Ver. 0.1.0, formatted

--- lib/swipeable_button.dart
+++ lib/swipeable_button.dart
@@ -4,7 +4,7 @@
 /// implement buttons that respond to swipe gestures. The button can be swiped
 /// to the right to perform an action, such as confirming a purchase or starting
 /// a voice call.
-/// 
+///
 /// Note that [SwipeableButton] should be used for critical actions only, such
 /// as deleting data or confirming an irreversible action.
 ///
@@ -35,7 +35,6 @@ import 'package:flutter/material.dart';
 /// A button that triggers an action on swipe.
 @immutable
 class SwipeableButton extends StatefulWidget {
-  
   /// The action triggered by the button on swipe.
   final VoidCallback onSwipe;
 
@@ -49,7 +48,7 @@ class SwipeableButton extends StatefulWidget {
   final bool oneTime;
 
   /// The label of the button.
-  /// 
+  ///
   /// This widget will be shown below the thumb.
   final Widget? label;
 
@@ -60,11 +59,12 @@ class SwipeableButton extends StatefulWidget {
   /// [context], the amount of swipe applied to the button (from 0.0 to
   /// 1.0) as [swipedFraction] and whether the swipe has been completed at least
   /// once as [hasSwiped].
-  final Widget Function(BuildContext context, double swipedFraction, bool hasSwiped) thumbBuilder;
+  final Widget Function(
+      BuildContext context, double swipedFraction, bool hasSwiped) thumbBuilder;
 
   /// The border radius of the button.
   final BorderRadius? borderRadius;
-  
+
   const SwipeableButton({
     Key? key,
     required this.onSwipe,
@@ -75,7 +75,7 @@ class SwipeableButton extends StatefulWidget {
     this.color,
     required this.thumbBuilder,
     this.borderRadius,
-  }): super(key: key);
+  }) : super(key: key);
 
   /// Creates a [SwipeableButton] with a simplified constructor. It uses a
   /// default thumb, but the [thumbColor] can be changed. If no [thumbColor] has
@@ -90,113 +90,126 @@ class SwipeableButton extends StatefulWidget {
     this.color,
     Color? thumbColor,
     this.borderRadius,
-  }) :
-    thumbBuilder = (
-      (BuildContext context, double swipedFraction, bool hasSwiped) => DecoratedBox(
-        decoration: BoxDecoration(color: thumbColor ?? Theme.of(context).primaryColor),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            SizedBox(
-              width: minThumbWidth,
-              child: hasSwiped && oneTime
-                ? const Icon(Icons.check, color: Colors.white)
-                : const Icon(Icons.chevron_right, color: Colors.white),
-            ),
-          ],
-        ),
-      )
-    ),
-    super(key: key);
+  })  : thumbBuilder = ((BuildContext context, double swipedFraction,
+                bool hasSwiped) =>
+            DecoratedBox(
+              decoration: BoxDecoration(
+                  color: thumbColor ?? Theme.of(context).primaryColor),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  SizedBox(
+                    width: minThumbWidth,
+                    child: hasSwiped && oneTime
+                        ? const Icon(Icons.check, color: Colors.white)
+                        : const Icon(Icons.chevron_right, color: Colors.white),
+                  ),
+                ],
+              ),
+            )),
+        super(key: key);
 
   @override
   State<SwipeableButton> createState() => _SwipeableButtonState();
-  
 }
 
 class _SwipeableButtonState extends State<SwipeableButton> {
-  
   double? draggingPosition;
   bool hasSwiped = false;
-  
+
   bool get isDragging => null != draggingPosition;
-  
-  double get thumbWidth => (null == draggingPosition) ? widget.minThumbWidth : (draggingPosition! + widget.minThumbWidth * 0.5);
+
+  double get thumbWidth => (null == draggingPosition)
+      ? widget.minThumbWidth
+      : (draggingPosition! + widget.minThumbWidth * 0.5);
 
   void updateDraggingPosition(double? horizontalPosition, double width) {
-    if(hasSwiped && widget.oneTime) {
+    if (hasSwiped && widget.oneTime) {
       return;
     }
-    draggingPosition = (null == horizontalPosition) ? null : horizontalPosition.clamp(widget.minThumbWidth * 0.5, width - widget.minThumbWidth * 0.5);
-    if(!hasSwiped && (horizontalPosition ?? -double.infinity) >= width - widget.minThumbWidth * 0.5) {
+    draggingPosition = (null == horizontalPosition)
+        ? null
+        : horizontalPosition.clamp(
+            widget.minThumbWidth * 0.5, width - widget.minThumbWidth * 0.5);
+    if (!hasSwiped &&
+        (horizontalPosition ?? -double.infinity) >=
+            width - widget.minThumbWidth * 0.5) {
       widget.onSwipe();
       hasSwiped = true;
     }
   }
-  
+
   @override
   Widget build(BuildContext context) => LayoutBuilder(
-    builder: (BuildContext context, BoxConstraints constraints) => SizedBox(
-      width: constraints.maxWidth,
-      height: widget.height,
-      child: GestureDetector(
-        onHorizontalDragStart: (DragStartDetails details) {
-          setState(() {
-            if(!isDragging && details.localPosition.dx > widget.minThumbWidth) {
-              return;
-            }
-            updateDraggingPosition(details.localPosition.dx, constraints.maxWidth);
-          });
-        },
-        onHorizontalDragUpdate: (DragUpdateDetails details) {
-          setState(() {
-            if(isDragging) {
-              updateDraggingPosition(details.localPosition.dx, constraints.maxWidth);
-            }
-          });
-        },
-        onHorizontalDragEnd: (DragEndDetails details) {
-          setState(() {
-            updateDraggingPosition(null, constraints.maxWidth);
-            if(!widget.oneTime) {
-              hasSwiped = false;
-            }
-          });
-        },
-        child: ClipRRect(
-          borderRadius: widget.borderRadius ?? BorderRadius.circular(5.0),
-          child: Stack(
-            children: <Widget>[
-              SizedBox(
-                width: constraints.maxWidth,
-                height: constraints.maxHeight,
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: widget.color ?? Theme.of(context).primaryColor,
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.only(left: widget.minThumbWidth),
-                    child: widget.label ?? Container(),
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+        builder: (BuildContext context, BoxConstraints constraints) => SizedBox(
+          width: constraints.maxWidth,
+          height: widget.height,
+          child: GestureDetector(
+            onHorizontalDragStart: (DragStartDetails details) {
+              setState(() {
+                if (!isDragging &&
+                    details.localPosition.dx > widget.minThumbWidth) {
+                  return;
+                }
+                updateDraggingPosition(
+                    details.localPosition.dx, constraints.maxWidth);
+              });
+            },
+            onHorizontalDragUpdate: (DragUpdateDetails details) {
+              setState(() {
+                if (isDragging) {
+                  updateDraggingPosition(
+                      details.localPosition.dx, constraints.maxWidth);
+                }
+              });
+            },
+            onHorizontalDragEnd: (DragEndDetails details) {
+              setState(() {
+                updateDraggingPosition(null, constraints.maxWidth);
+                if (!widget.oneTime) {
+                  hasSwiped = false;
+                }
+              });
+            },
+            child: ClipRRect(
+              borderRadius: widget.borderRadius ?? BorderRadius.circular(5.0),
+              child: Stack(
                 children: <Widget>[
-                  AnimatedContainer(
-                    duration: isDragging ? const Duration() : const Duration(milliseconds: 200),
-                    curve: Curves.easeOut,
-                    width: thumbWidth,
-                    child: widget.thumbBuilder(context, (widget.minThumbWidth - thumbWidth) / (widget.minThumbWidth - constraints.maxWidth), hasSwiped),
+                  SizedBox(
+                    width: constraints.maxWidth,
+                    height: constraints.maxHeight,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: widget.color ?? Theme.of(context).primaryColor,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: widget.minThumbWidth),
+                        child: widget.label ?? Container(),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      AnimatedContainer(
+                        duration: isDragging
+                            ? const Duration()
+                            : const Duration(milliseconds: 200),
+                        curve: Curves.easeOut,
+                        width: thumbWidth,
+                        child: widget.thumbBuilder(
+                            context,
+                            (widget.minThumbWidth - thumbWidth) /
+                                (widget.minThumbWidth - constraints.maxWidth),
+                            hasSwiped),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
-      ),
-    ),
-  );
-  
+      );
 }
